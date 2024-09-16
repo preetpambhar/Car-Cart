b712105dbae175f60602cb17caa57b2f1956ba27
Preet Pambhar <preetpambhar73530@gmail.com>
2024-09-16 00:24:24 -0400

    Initial Commit

---
+++ Car Toys/Car_ToysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_ToysApp.swift
+//  Car Toys
+//
+//  Created by Preet Pambhar on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct Car_ToysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
