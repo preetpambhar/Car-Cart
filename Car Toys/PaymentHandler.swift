90ec5f32ae38a51b89fbe97cccdd67380e5e3d6d
Preet Pambhar <preetpambhar73530@gmail.com>
2024-09-20 01:43:40 -0400

    feat: shippingMethod

---
+++ Car Toys/PaymentHandler.swift
@@ -0,0 +1,44 @@
+//
+//  PaymentHandler.swift
+//  Car Toys
+//
+//  Created by Preet Pambhar on 2024-09-20.
+//
+
+import Foundation
+import PassKit
+
+typealias PaymentCompletionHandler = (Bool) -> Void
+
+class PaymentHandler: NSObject {
+    var paymentController: PKPaymentAuthorizationController?
+    var paymentSummaryItems = [PKPaymentSummaryItem]()
+    var paymentStatus =  PKPaymentAuthorizationStatus.failure
+    var completionHandler: PaymentCompletionHandler?
+    
+    static let supportedNetworks: [PKPaymentNetwork] = [
+        .visa,
+        .masterCard,
+    ]
+    
+    func shippingMethodCalculator() -> [PKShippingMethod]{
+        let today = Date()
+        let calendar = Calendar.current
+        let shippingStart = calendar.date(byAdding: .day, value: 5, to: today)
+        let shippingEnd = calendar.date(byAdding: .day, value: 10, to: today)
+        
+        if let shippingEnd = shippingEnd, let shippingStart = shippingStart{
+            let startComponents = calendar.dateComponents([.calendar, .year, .month, .day], from: shippingStart)
+            let endComponents = calendar.dateComponents([.calendar, .year, .month, .day], from: shippingEnd)
+            
+            let shippingDelivery = PKShippingMethod(label: "Delivery", amount: NSDecimalNumber(string: "0.00"))
+            shippingDelivery.dateComponentsRange = PKDateComponentsRange(start: startComponents, end: endComponents)
+            shippingDelivery.detail = "Car sent to your address"
+            shippingDelivery.identifier = "DELIVERY"
+            
+            return [shippingDelivery]
+        }
+        return []
+    }
+    
+}
